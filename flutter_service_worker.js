a9ab309838b4acb3eaac4cfa2a44f4036fbd94f5
Javier-dlp <Javier-dlp@users.noreply.github.com>
2021-08-07 08:27:14 +0000

    deploy: 06613339f0c5263160c17417014b5a3c45a5ec7d

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -16,8 +16,8 @@ const RESOURCES = {
 "manifest.json": "aa60395a507819c8654162eed0688809",
 "version.json": "177777c9ef1f59e02f9ebe8b0f187e4d",
 "main.dart.js": "70daefd163b127dbc26437d53a212bff",
-"index.html": "af4b47b4c98db631a844b5be244bdbba",
-"/": "af4b47b4c98db631a844b5be244bdbba"
+"index.html": "e887b5f31ba1878ae015f902553dad9e",
+"/": "e887b5f31ba1878ae015f902553dad9e"
 };
 
 // The application shell files that are downloaded before a service worker can
